f88d6ef62f33bc2ca4af8ae9dfa8620c01bea1ac
Yatika Dewaskar <yatikadewaskar04@gmail.com>
2024-07-31 12:11:53 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+// app.listen(3000 , () =>{
+//     console.log("app is running successfully");
+// });
+
+// app.get("/",(req,res) =>{
+//     res.send('<h1>This is Homepage</h1>');
+// })
+
